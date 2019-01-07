506204019e1515f3001a3f32eb360da8a558130d
Dorota Kopczyk <dkopczyk90@gmail.com>
2019-01-07 00:12:45 -0600

    Clean up controller, play around with some pagination helpers and the like

---
+++ GoogleLoginApp/SampleMVCApp.Api/Controllers/v2/FoodController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMVCApp.Api.Controllers.v2
+{
+    //There isn't anything here, its just to show that there could be
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class FoodController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("2.0");
+        }
+    }
+}
