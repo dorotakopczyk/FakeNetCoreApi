568dad1ab95b7a0e6d3da57ac97c687f336e38f4
Dorota Kopczyk <dkopczyk90@gmail.com>
2019-01-07 00:45:14 -0600

    add deployment and fix rename

--- GoogleLoginApp/SampleMVCApp.Api/Services/DataSeeder.cs
+++ GoogleLoginApp/SampleMVCApp.Api/Services/DataSeeder.cs
@@ -9,14 +9,14 @@ namespace SampleMVCApp.Api.Services
     {
         public async Task Initialize(FoodDbContext context)
         {
-            context.FoodItems.Add(new FoodItem() { Calories = 250, Name = "Apple", Type = "Fruit", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodItem() { Calories = 500, Name = "Turkey Leg", Type = "Main", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodItem() { Calories = 750, Name = "Avocado", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodItem() { Calories = 1000, Name = "Cheesecake",Type = "Dessert", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodItem() { Calories = 250, Name = "Banana", Type = "Fruit", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodItem() { Calories = 500, Name = "Shepherds Pie", Type = "Main", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodItem() { Calories = 750, Name = "Filet Mignon", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodItem() { Calories = 1000, Name = "Chana Masala", Type = "Main", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 250, Name = "Apple", Type = "Fruit", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 500, Name = "Turkey Leg", Type = "Main", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 750, Name = "Avocado", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 1000, Name = "Cheesecake",Type = "Dessert", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 250, Name = "Banana", Type = "Fruit", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 500, Name = "Shepherds Pie", Type = "Main", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 750, Name = "Filet Mignon", Created = DateTime.Now });
+            context.FoodItems.Add(new Food() { Calories = 1000, Name = "Chana Masala", Type = "Main", Created = DateTime.Now });
             await context.SaveChangesAsync();
         }
     }
